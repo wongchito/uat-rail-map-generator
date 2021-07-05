7484ef7d3d03fa12c77bcc20a98fde756ce63f27
Release Agent <rmg.release.agent@users.noreply.github.com>
2021-07-05 06:16:35 +0000

    Release RMG version 3.8.34.GH-88-TUEN-MA-LINE.ed2fcd4 to UAT

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uat-rail-map-generator/precache-manifest.59993a47b812d778f8dbd16c9b45ba4b.js"
+  "/uat-rail-map-generator/precache-manifest.f4f792883805be7a8682f25d5361110d.js"
 );
 
 self.addEventListener('message', (event) => {
