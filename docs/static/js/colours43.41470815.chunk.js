e453fb7b00042a2c34745deae29a5b2f92da753f
Release Agent <rmg.release.agent@users.noreply.github.com>
2021-07-14 14:44:24 +0000

    Release RMG version 3.9.3 to UAT

--- docs/static/js/colours43.41470815.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[19],{57:function(a,e,t){"use strict";t.r(e);e.default=[{id:"other",name:{en:"Customise","zh-Hans":"\u81ea\u5b9a\u4e49","zh-Hant":"\u81ea\u8a02"},colour:"#aaaaaa"}]}}]);
-//# sourceMappingURL=colours43.41470815.chunk.js.map
