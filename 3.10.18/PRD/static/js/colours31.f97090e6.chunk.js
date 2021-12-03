69769db8ded5083445dbd2704dc1700dc3a27159
Build Agent <rmg.build.agent@users.noreply.github.com>
2021-12-03 08:21:11 +0000

    Build RMG version 3.10.18

---
+++ 3.10.18/PRD/static/js/colours31.f97090e6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[16],{202:function(n,a,e){"use strict";e.r(a);a.default=[{id:"hrb1",name:{en:"Line 1","zh-Hans":"1\u53f7\u7ebf","zh-Hant":"1\u865f\u7dda"},colour:"#C8102E"},{id:"hrb2",name:{en:"Line 2","zh-Hans":"2\u53f7\u7ebf","zh-Hant":"2\u865f\u7dda"},colour:"#44AF52"},{id:"hrb3",name:{en:"Line 3","zh-Hans":"3\u53f7\u7ebf","zh-Hant":"3\u865f\u7dda"},colour:"#FFC72C"}]}}]);
+//# sourceMappingURL=colours31.f97090e6.chunk.js.map
---
+++ 3.10.18/UAT/static/js/colours91.0d234c82.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[49],{232:function(e,a,r){"use strict";r.r(a);var n=r(9),i=r(6),l=[{id:"yellow",name:Object(n.a)({},i.f.English,"Yellow Line"),colour:"#FABF00"},{id:"green",name:Object(n.a)({},i.f.English,"Green Line"),colour:"#0EAB4A"},{id:"bus",name:Object(n.a)({},i.f.English,"Bus"),colour:"#e9172a"},{id:"ferry",name:Object(n.a)({},i.f.English,"Ferry"),colour:"#82CEF3"}];a.default=l}}]);
+//# sourceMappingURL=colours91.0d234c82.chunk.js.map
