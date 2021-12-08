101dcf7f8418f3076ad6d1aa38c246e118fdee95
Build Agent <rmg.build.agent@users.noreply.github.com>
2021-12-08 16:36:31 +0000

    Build RMG version 5.0.0-0.v5.main.b65a457

---
+++ 5.0.0-0.v5.main.b65a457/UAT/static/js/colours27.88edb1a3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[14],{361:function(e,r,a){"use strict";a.r(r);var n=a(3),i=a(16),c=[{id:"inner",name:Object(n.a)({},i.f.English,"Inner Circle"),colour:"#4f4c4d"},{id:"outer",name:Object(n.a)({},i.f.English,"Outer Circle"),colour:"#f57c14"}];r.default=c}}]);
+//# sourceMappingURL=colours27.88edb1a3.chunk.js.map
