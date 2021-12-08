5215975b7ea0870453a3ac81d521e979231acdfe
Release Agent <rmg.release.agent@users.noreply.github.com>
2021-12-08 16:37:55 +0000

    Release RMG version 5.0.0-0.v5.main.b65a457 to UAT

--- docs/static/js/colours27.d7fd0503.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[14],{353:function(e,r,a){"use strict";a.r(r);var n=a(4),i=a(16),c=[{id:"inner",name:Object(n.a)({},i.f.English,"Inner Circle"),colour:"#4f4c4d"},{id:"outer",name:Object(n.a)({},i.f.English,"Outer Circle"),colour:"#f57c14"}];r.default=c}}]);
-//# sourceMappingURL=colours27.d7fd0503.chunk.js.map
---
+++ docs/static/js/colours59.fba3b84a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[32],{377:function(a,e,t){"use strict";t.r(e);var i,r=t(3),s=t(16),n=[{id:"other",name:(i={},Object(r.a)(i,s.f.English,"Customise"),Object(r.a)(i,s.f.ChineseSimp,"\u81ea\u5b9a\u4e49"),Object(r.a)(i,s.f.ChineseTrad,"\u81ea\u8a02"),i),colour:"#aaaaaa"}];e.default=n}}]);
+//# sourceMappingURL=colours59.fba3b84a.chunk.js.map
