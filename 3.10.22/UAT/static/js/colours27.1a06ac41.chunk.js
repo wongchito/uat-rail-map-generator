8aae921b68a3018852fb96438ac51b7f13bee7d0
Build Agent <rmg.build.agent@users.noreply.github.com>
2021-12-11 10:17:30 +0000

    Build RMG version 3.10.22

---
+++ 3.10.22/UAT/static/js/colours27.1a06ac41.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprail-map-generator"]=this["webpackJsonprail-map-generator"]||[]).push([[14],{193:function(e,r,a){"use strict";a.r(r);var n=a(9),i=a(6),c=[{id:"inner",name:Object(n.a)({},i.f.English,"Inner Circle"),colour:"#4f4c4d"},{id:"outer",name:Object(n.a)({},i.f.English,"Outer Circle"),colour:"#f57c14"}];r.default=c}}]);
+//# sourceMappingURL=colours27.1a06ac41.chunk.js.map
