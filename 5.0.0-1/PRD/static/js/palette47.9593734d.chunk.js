d5db9b0f35e1838b2443c08a0eb7e9d5f05155cc
Build Agent <rmg.build.agent@users.noreply.github.com>
2022-01-23 06:39:52 +0000

    Build RMG version 5.0.0-1

---
+++ 5.0.0-1/PRD/static/js/palette47.9593734d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrail_map_generator=self.webpackChunkrail_map_generator||[]).push([[111],{9314:function(e,n,r){r.r(n);n.default=[{id:"yellow",name:{en:"Yellow Line"},colour:"#FABF00"},{id:"green",name:{en:"Green Line"},colour:"#0EAB4A"},{id:"bus",name:{en:"Bus"},colour:"#e9172a"},{id:"ferry",name:{en:"Ferry"},colour:"#82CEF3"}]}}]);
+//# sourceMappingURL=palette47.9593734d.chunk.js.map
