0ce3bf87189b4ee532bee4c7abd000338d86b86a
Release Agent <rmg.release.agent@users.noreply.github.com>
2022-03-13 13:12:27 +0000

    Release RMG version 5.0.0-7 to UAT

--- docs/static/js/palette17.9a6ed83a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrail_map_generator=self.webpackChunkrail_map_generator||[]).push([[5053],{4476:function(e,r,n){n.r(r);r.default=[{id:"inner",name:{en:"Inner Circle"},colour:"#4f4c4d"},{id:"outer",name:{en:"Outer Circle"},colour:"#f57c14"}]}}]);
-//# sourceMappingURL=palette17.9a6ed83a.chunk.js.map
